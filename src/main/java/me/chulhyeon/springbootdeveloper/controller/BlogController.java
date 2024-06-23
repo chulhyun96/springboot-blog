10512e59658b379f6732042923202761768e564d
박철현 <tghy4@naver.com>
2024-06-23 21:23:08 +0900

    update

--- src/main/java/me/chulhyeon/springbootdeveloper/controller/BlogController.java
+++ src/main/java/me/chulhyeon/springbootdeveloper/controller/BlogController.java
@@ -23,13 +23,14 @@ public class BlogController {
 
     @PostMapping("/api/articles")
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
-        log.info("api Article 호출");
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
     }
+
     @GetMapping("/api/articles")
     public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        log.info("api findAll 호출");
         List<ArticleResponse> articles = blogService.findAll()
                 .stream()
                 .map(article -> new ArticleResponse(article.getTitle(), article.getContent()))
