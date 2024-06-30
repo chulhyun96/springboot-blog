663dcbcadae5ae5f971dae166b20a773c715f0a6
박철현 <tghy4@naver.com>
2024-06-30 20:07:09 +0900

    DELETE MAPPING을 처리할 자바 스크립트 작성

--- src/main/java/me/chulhyeon/springbootdeveloper/controller/api/BlogController.java
+++ src/main/java/me/chulhyeon/springbootdeveloper/controller/api/BlogController.java
@@ -1,4 +1,4 @@
-package me.chulhyeon.springbootdeveloper.controller;
+package me.chulhyeon.springbootdeveloper.controller.api;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
--- src/main/java/me/chulhyeon/springbootdeveloper/dto/ArticleListViewResponse.java
+++ src/main/java/me/chulhyeon/springbootdeveloper/dto/ArticleListViewResponse.java
@@ -1,17 +1,23 @@
 package me.chulhyeon.springbootdeveloper.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import me.chulhyeon.springbootdeveloper.domain.Article;
 
+import java.time.LocalDateTime;
+
 @Getter
+@NoArgsConstructor
 public class ArticleListViewResponse {
-    private final Long id;
-    private final String title;
-    private final String content;
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
 
     public ArticleListViewResponse(Article article) {
         this.id = article.getId();
         this.title = article.getTitle();
         this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
     }
 }
